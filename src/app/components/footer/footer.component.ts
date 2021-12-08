2ec7d18b6e3d5cbd3e785c0e0c41dd1f8172124b
Christoph <christoph.parrez@gmail.com>
2021-12-08 16:30:46 +0100

    Changes by Wouter

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  public _year: Date;
+
+  constructor() {
+    this._year = new Date();
+  }
 
   ngOnInit(): void {
   }
