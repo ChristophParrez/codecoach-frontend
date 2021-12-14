ad8738980d87cb884efb3f75be73b69e47043bbb
Christoph <christoph.parrez@gmail.com>
2021-12-14 16:42:32 +0100

    Coach Profile

--- src/app/components/coach-profile/coach-profile.component.ts
+++ src/app/components/coach-profile/coach-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from "../../services/user.service";
 import { ActivatedRoute } from "@angular/router";
+import { User } from "../../model/User";
 
 @Component({
   selector: 'app-coach-profile',
@@ -9,7 +10,7 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class CoachProfileComponent implements OnInit {
 
-  user: any;
+  user: User | undefined;
 
   constructor(public userService: UserService, private route: ActivatedRoute) {
   }
