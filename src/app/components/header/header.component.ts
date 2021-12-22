3d75a7a64c5ef05af87ffebe309fc56ac4a7e471
Wouter Biront <wouterbiront@gmail.com>
2021-12-22 11:15:45 +0100

    Add x-mas wishes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {faGithub} from '@fortawesome/free-brands-svg-icons';
 import {UserService} from "../../services/user.service";
+import {MatDialog} from "@angular/material/dialog";
+import {AlertDialogComponent, AlertDialogData} from "../dialogs/alert-dialog/alert-dialog.component";
 
 @Component({
   selector: 'app-header',
@@ -12,13 +14,23 @@ export class HeaderComponent implements OnInit {
   @Output() openSidenav = new EventEmitter<any>();
   gitHubIcon = faGithub;
 
-  constructor(public userService: UserService) {
+  constructor(public userService: UserService,
+              public dialog: MatDialog) {
   }
 
   ngOnInit(): void {
     if (this.userService.isLoggedIn()) {
       this.userService.getUserName();
     }
+  }
+
+  holidays() {
+    const dialogData: AlertDialogData = {title: "Merry X-mas & Happy New Year", message: 'The Codecoach team wishes you a Merry Christmas and a Happy New Year'};
+    this.dialog.open(AlertDialogComponent, {
+      maxWidth: "600px",
+      data: dialogData,
+      autoFocus: false
+    });
   }
 
 }
